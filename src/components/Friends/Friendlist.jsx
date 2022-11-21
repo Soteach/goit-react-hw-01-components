3d29149709f29c92b89579c96cc171530433e388
Sergij Suprunenko <104130197+Soteach@users.noreply.github.com>
2022-11-21 14:44:45 +0200

    friendlist rem

--- src/components/Friends/Friendlist.jsx
+++ src/components/Friends/Friendlist.jsx
@@ -1,20 +1,21 @@
-// import PropTypes from 'prop-types';
-// import { FriendItem } from './FriendList.styled';
-// function FriendList({ friends }) {
-//   return (
-//     <>
-//       <FriendList>
-//         <h2>Friendlist</h2>
-//         {/* {friends.map(({ avatar, name, isOnline, id }) => (
-//           <FriendItem key={id}>
-//             <span class="status">{isOnline}</span>
-//             <img class="avatar" src={avatar} alt="User avatar" width="48" />
-//             <p class="name"></p>
-//           </FriendItem>
-//         ))} */}
-//       </FriendList>
-//     </>
-//   );
-// }
+import PropTypes from 'prop-types';
+import { FriendItem } from './FriendList.styled';
 
-// export default FriendList;
+function FriendList({ friends }) {
+  return (
+    <>
+      <FriendList>
+        <h2>Friendlist</h2>
+        {friends.map(({ avatar, name, isOnline, id }) => (
+          <FriendItem key={id}>
+            <span class="status">{isOnline}</span>
+            <img class="avatar" src={avatar} alt="User avatar" width="48" />
+            <p class="name">{name}</p>
+          </FriendItem>
+        ))}
+      </FriendList>
+    </>
+  );
+}
+
+export default FriendList;
